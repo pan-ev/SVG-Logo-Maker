33b9a9072219d27cff726a786d513b592dee072c
epan <epan616@gmail.com>
2023-04-09 21:38:30 -0400

    Add comments and inquirer maxlength

--- README.md
+++ README.md
@@ -1,1 +1,8 @@
 # SVG-Logo-Maker
+
+## Description
+
+The purpose of this project is to be able to generate a logo using a command-line application.    
+
+The application prompts the user to select text, text color, a shape, and shape color. The application generates an SVG file with a logo that reflect the choices of the user. 
+
--- index.js
+++ index.js
@@ -2,11 +2,17 @@ const inquirer = require("inquirer");
 const fs = require("fs");
 const genShape = require("./lib/shapes.js");
 
+// Add ability to set max length on inquirer prompt
+const MaxLengthInputPrompt = require('inquirer-maxlength-input-prompt')
+inquirer.registerPrompt('maxlength-input', MaxLengthInputPrompt)
+
+// Inquirer questions
 const questions = [
   {
-    type: "input",
+    type: "maxlength-input",
     name: "text",
     message: "Please enter your logo text (3 char)",
+    maxLength: 3
   },
   {
     type: "input",
@@ -26,8 +32,11 @@ const questions = [
   },
 ];
 
+// Function to generate the SVG file
 function writeToFile(fileName, data) {
   var svgShape;
+
+//   Call applicable shape class based on which shape user selects
   if (data.shape == "triangle") {
     const shape = new genShape.Triangle(data.shape, data.shapeColor);
     svgShape = shape.render(data.shapeColor);
@@ -39,6 +48,7 @@ function writeToFile(fileName, data) {
     svgShape = shape.render(data.shapeColor);
   }
 
+//   Generate the SVG content based on user inputs
   var svgText = `<svg version="1.1"
   width="300" height="200"
   xmlns="http://www.w3.org/2000/svg">
@@ -55,6 +65,7 @@ ${svgShape}
   );
 }
 
+// Run inquirer and write to file on initialization
 function init() {
   inquirer.prompt(questions).then((data) => {
     writeToFile("logo.svg", data);
